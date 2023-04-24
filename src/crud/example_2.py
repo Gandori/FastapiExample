82a11da0d8d3f39b89ec8211e74e5c876261f129
gandori <marcel.stiebing@web.de>
2023-04-24 03:57:10 +0200

    chore: delete example_2

--- src/crud/example_2.py
+++
@@ -1,7 +0,0 @@
-from src.schemas.example_2 import Example2
-
-
-class Example2Grud:
-    @staticmethod
-    def example(*, name: str) -> Example2:
-        return Example2(name=name)
--- src/schemas/example_2.py
+++
@@ -1,5 +0,0 @@
-from pydantic import BaseModel
-
-
-class Example2(BaseModel):
-    name: str
